2cc6dec4a6630bd711d97ce8c5c558c4effa89e3
Saila <saila.sms96@gmail.com>
2019-07-07 18:59:39 -0400

    defined fiber grid

--- simulation/objects.py
+++ simulation/objects.py
@@ -7,30 +7,38 @@ class Fiber:
     core_index = 1.4630  # Refractive index 88nm RI.info
     cladding_index = 1.45
     surrounding_index = 1
-    ellipse_a = 10e-6  # semi-major axis of fiber cross section
+    ellipse_a = 100e-6  # semi-major axis of fiber cross section
     ellipse_b = 100e-6  # semi-minor axis of mmf cross section
-    length = 8000e-6  # length of fiber in microns. 8000 um = 8 mm):
+    length = 12000e-6  # length of fiber in microns. 8000 um = 8 mm):
+
+    # plot cylinder source: https://stackoverflow.com/questions/26989131/add-cylinder-to-plot
+    # spherical tutorial: https://stackoverflow.com/questions/36816537/spherical-coordinates-plot-in-matplotlib
+    # cylindrical tutorial: https://matplotlib.org/3.1.0/gallery/mplot3d/voxels_torus.html
+
+    theta = np.linspace(0, np.pi * 2, 1000)  # assuming 50 um max
+    z = np.linspace(0, length, 1000)
+    theta_grid, z_grid = np.meshgrid(theta, z)
+    # wikipedia page on ellipse: https://en.wikipedia.org/wiki/Ellipse#Polar_form_relative_to_center
+    r_grid = ellipse_a * ellipse_b / (np.sqrt((ellipse_b * np.cos(theta_grid)) ** 2 + (ellipse_a * np.sin(theta_grid)) ** 2))
+    x_grid = r_grid * np.cos(theta_grid)
+    y_grid = r_grid * np.sin(theta_grid)
 
     def draw(self, fig):
-        # plot cylinder source: https://stackoverflow.com/questions/26989131/add-cylinder-to-plot
-        # spherical tutorial: https://stackoverflow.com/questions/36816537/spherical-coordinates-plot-in-matplotlib
-        # cylindrical tutorial: https://matplotlib.org/3.1.0/gallery/mplot3d/voxels_torus.html
-
-        theta = np.linspace(0, np.pi*2, 1000)# assuming 50 um max
-        z = np.linspace(-50e-6, self.length, 1000)
-        theta_grid, z_grid = np.meshgrid(theta, z)
-        # wikipedia page on ellipse: https://en.wikipedia.org/wiki/Ellipse#Polar_form_relative_to_center
-        r_grid = self.ellipse_a * self.ellipse_b /(np.sqrt((self.ellipse_b * np.cos(theta_grid)) ** 2 + (self.ellipse_a * np.sin(theta_grid)) ** 2))
-        x_grid = r_grid * np.cos(theta_grid)
-        y_grid = r_grid * np.sin(theta_grid)
 
         ax = fig.gca(projection='3d')
-        ax.plot_surface(x_grid, y_grid, z_grid, alpha = 0.1)
+        ax.plot_surface(self.x_grid, self.y_grid, self.z_grid, alpha = 0.1)
+
+        ax.set(xlim=(-1.5 * self.ellipse_a, 1.5 * self.ellipse_a), ylim = (-1.5 * self.ellipse_b , 1.5 * self.ellipse_b ), zlim = (-100e-6 , 1.2 * self.length))
+        ax.set_xlabel("X")
+        ax.set_ylabel("Y")
+        ax.set_zlabel("Z")
+
 
     def reflect(self, ray):
         intersection = np.array([0,0,0])
         # start point of reflected ray =  intersection point of previous point and wall of fiber
-        # consider edge case top face of fiber
+        # consider edge case top face of fiber and bottom face of fiber
+
 
         return Ray([intersection], theta, psi)
 
