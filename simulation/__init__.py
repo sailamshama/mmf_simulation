fcdace1236878f463a0ed029c0dcf77f948e314a
Saila <saila.sms96@gmail.com>
2019-07-09 03:29:59 -0400

    Visualized reflected ray.

--- simulation/__init__.py
+++ simulation/__init__.py
@@ -4,6 +4,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def generate_rays_multiple_sources(initial_points, samples, psi_cutoff=math.pi):
+    #TODO: fix this too
     rays_multiple_points = np.array([])
     for initial_point in initial_points:
         rays_single_point = generate_rays_single_source(initial_point, psi_cutoff, samples)
@@ -30,7 +31,6 @@ def generate_rays_single_source(initial_point, psi_cutoff=math.pi, samples=1000)
             rays = rays[:i]
             break
         theta = ((i + rnd) % samples) * increment #azimuthal (projection) angle
-
         rays[i].theta = theta
         rays[i].psi = psi
 
@@ -48,26 +48,21 @@ if __name__ == '__main__':
     fiber = Fiber()
     init_points = np.array([[-50e-6, 0e-6, 0e-6]])
     # https://circuitglobe.com/numerical-aperture-of-optical-fiber.html
-    # psi_max = np.arcsin(fiber.surrounding_index * fiber.NA / fiber.core_index)
-    psi_max = np.pi/ 2
-    # generated_rays = generate_rays_multiple_sources(init_points, 100000, psi_max)
-    generated_rays = generate_rays_single_source(np.squeeze(init_points), psi_max, 1000)
+    psi_max = np.arcsin(fiber.surrounding_index * fiber.NA / fiber.core_index)
+    generated_rays = generate_rays_single_source(init_points, psi_max, 100)
 
-    # TODO: debug this
-    points = np.zeros((generated_rays.size, 3))
-    for i, ray in enumerate(generated_rays):
-        points[i] = np.array([np.sin(ray.psi) * np.cos(ray.theta), np.sin(ray.psi) * np.sin(ray.theta), np.cos(ray.psi)])
-        print(points[i])
+    fig = plt.figure()
+    fiber.draw(fig)
+    ray = Ray(np.array([0, 0, 0]), 0, psi= np.pi/2)
+    final_point = fiber.get_intersection(ray)
+    ray.draw(fig, final_point)
 
-    generated_rays_figure = plt.figure()
-    ax = plt.gca(projection='3d')
-    ax.set_xlim3d(-1, 1)
-    ax.set_ylim3d(-1, 1)
-    ax.set_zlim3d(-1, 1)
-    ax.scatter3D(points[:,0], points[:,1], points[:, 2], zdir='z', s=1, c=None)
+    reflected_ray = fiber.reflect(ray)
+    final_point_reflected_ray = fiber.get_intersection(reflected_ray)
+    reflected_ray.draw(fig, final_point_reflected_ray)
     plt.show()
-    plt.close(generated_rays_figure)
 
-    end_points = fiber.propagate(generated_rays)
 
+
+    t=1
 
