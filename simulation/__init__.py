9bde89d5c5129d148dd7cec6f2e989ed8101df4d
Saila <saila.sms96@gmail.com>
2019-08-14 21:48:14 -0400

    latest copy on laptop

--- simulation/__init__.py
+++ simulation/__init__.py
@@ -6,7 +6,7 @@ import time
 from simulation.bead import Bead
 from mpl_toolkits.mplot3d import Axes3D
 
-DRAW = True
+DRAW = False
 FILENAME = 'mid'
 BIN_SIZE = 150
 
@@ -21,36 +21,49 @@ if __name__ == '__main__':
         fiber.draw(fig1)
 
     beads = [
-        # Bead(np.array([0e-6, 0e-6, 0e-6])),
+        Bead(np.array([0e-6, 0e-6, 0e-6])),
+        Bead(np.array([0e-6, 10e-6, 0e-6])),
+        Bead(np.array([0e-6, 20e-6, 0e-6])),
+        Bead(np.array([0e-6, 30e-6, 0e-6])),
+        Bead(np.array([0e-6, 40e-6, 0e-6])),
         Bead(np.array([0, 51e-6, -45e-6])),
-        # Bead(np.array([75e-6, 0, 0])),
-        # Bead(np.array([10e-6, 0, 0]))
+        Bead(np.array([0, 51e-6, 0e-6])),
+        Bead(np.array([0e-6, 60e-6, 0e-6])),
+        Bead(np.array([0e-6, 70e-6, 0e-6])),
+        Bead(np.array([0e-6, 80e-6, 0e-6])),
+        Bead(np.array([0e-6, 90e-6, 0e-6])),
+        Bead(np.array([0e-6, 99e-6, 0e-6])),
+        Bead(np.array([10e-6, 0, 0]))
     ]
 
-    nums = int(1e6)
+    nums = int(1e6 )
     rays = np.array([])
+
+    print(time.time() - start)
     for bead in beads:
+        start = time.time()
+        rays = np.array([])
         rays = np.append(rays, bead.generate_rays(fiber, nums))
         if DRAW:
             bead.draw(fig1)
 
-    final_positions = fiber.propagate(rays[0], fig1, draw=DRAW)
-    for i in range(1, rays.size):
-        final_positions = np.vstack((final_positions, fiber.propagate(rays[i], fig1, draw=DRAW)))
-        if ((i - 1) * 100) % rays.size > (i * 100) % rays.size:
-            print('progress: ', int((i / rays.size) / 0.01), '%')
-    if DRAW:
-        fig1.show()
+        final_positions = fiber.propagate(rays[0], fig1, draw=DRAW)
+        for i in range(1, rays.size):
+            final_positions = np.vstack((final_positions, fiber.propagate(rays[i], fig1, draw=DRAW)))
+            if ((i - 1) * 100) % rays.size > (i * 100) % rays.size:
+                print('progress: ', int((i / rays.size) / 0.01), '%')
+        if DRAW:
+            fig1.show()
 
-    heatmap, xedges, yedges = np.histogram2d(final_positions[:, 0], final_positions[:, 1], bins=BIN_SIZE)
-    extent = [xedges[0], xedges[-1], yedges[0], yedges[-1]]
-    plt.ticklabel_format(style='sci', axis='x', scilimits=(0, 0))
-    plt.ticklabel_format(style='sci', axis='y', scilimits=(0, 0))
-    plt.imshow(heatmap.T, extent=extent, origin='lower')
-    # plt.imsave(getcwd() + '/simulated_calibration/' + 'img_test_' + FILENAME + '.tiff', heatmap.T)
+        heatmap, xedges, yedges = np.histogram2d(final_positions[:, 0], final_positions[:, 1], bins=BIN_SIZE)
+        extent = [xedges[0], xedges[-1], yedges[0], yedges[-1]]
+        plt.ticklabel_format(style='sci', axis='x', scilimits=(0, 0))
+        plt.ticklabel_format(style='sci', axis='y', scilimits=(0, 0))
+        plt.imshow(heatmap.T, extent=extent, origin='lower')
+        plt.imsave(getcwd() + '/simulated_calibration/' + 'img_y'+ str(bead.position[1]) + '_test_'+ str(nums) + FILENAME + '.tiff', heatmap.T)
 
-    fig2.show()
+        fig2.show()
 
-    end = time.time()
+        end = time.time()
 
-    print("Simulation time: ", end-start)
+        print("Position:", bead.position[1], "Simulation time: ", end-start)
